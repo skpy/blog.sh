a8ba764eaf4f2e29db3e4c9f69ee661e7c079cad
Scott Merrill <smerrill@covermymeds.com>
2019-11-07 08:54:24 -0500

    update for new process

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Generate a (mostly) Markdown-powered blog. Source files are stored in Markdown, to be rendered on the fly by a web server like [Caddy](https://caddyserver.com/v1/docs/markdown).
 
-This supports `posts` and `notes`, where the former is long-form content with titles, and the latter is short-form microblog entries with no titles.
-
 In order to generate valid XML for RSS 2.0 feeds, `pandoc` is required to convert the Markdown to HTML for inclusion in the output feed file.
 
 ## Requirements
@@ -14,35 +12,31 @@ In order to generate valid XML for RSS 2.0 feeds, `pandoc` is required to conver
 ## Config
 A config file is mandatory. Copy the provided `config.SAMPLE` to `config` and edit appropriately.
 
+`SRC` is the directory in which new content will be found for processing.
+
 `DOCROOT` is the root directory for your website. All output files will be created here. No trailing slash.
 
 `URL` is the full URL of your site. No trailing slash.
 
 `DESCRIPTION` is the text to supply to the `<description>` element in the RSS feed(s).
 
-`SRC` is the directory in which new content will be found for processing.
+`HOMECOUNT` is the number of items to display on the home page.
+
+`RSSCOUNT` is the number of items to display in the RSS feed.
+
+## Custom Functions
+You may define custom functions inside the config file to alter the execution of some parts of this script. 
+
+`ARCHIVE_FUNC` is the name of a function that you declare in your config file.  This function will be invoked when building the archive list of all content. By default, `blog.sh` will create a full list of all content.  You can override this in your own function.
+
+`HOME_FUNC` is the name of a function tha you declare in your config file to alter how the home page is generated. By default, `blog.sh` will create a list of `HOMECOUNT` items, newest first.
 
 ## Usage
-`blog.sh` requires a command line argument to tell it what to do. 
-
-Valid options are:
-* post: process a new post
-* note: process a new note
-* home: generate the home page
-* rss: generate the index.xml RSS feed
-
-`post` and `note` each expect an appropriately named file in `SRC` to process. If any JPEG images are found in the source directory, they are copied to a sub-directory in `DOCROOT/images/`
+`blog.sh` requires a command line argument to tell it what config file to use. If no option is provided, it will look for a file named `config` in the current working directory.  If none is found, the script will exit with an error.
 
 ## Data Organization
-In addition to the output files created in `DOCROOT`, this script will create three additional files, stored in the same directory as this script:
+In addition to the output files created in `DOCROOT`, this script will create a file named `permalinks.txt` that contains a complete list of all permalinks for content processed by `blog.sh`.
 
-* `all.txt` a list of permalinks for all content
-* `notes.txt` a list of permalinks for all notes
-* `posts.txt` a list of permlinks for posts
-
-These three files are used to minimize the amount of `find`ing used in order to improve performance a little bit.
 
 ## How I use this
 I use the iOS Shortcuts application on my iPhone and iPad to take input from me, prompt for an opitonal image, upload all of that to my server, then invoke `blog.sh` via SSH.
-
-For notes, the Shortcuts prompts for input.  For posts, the Shortcut accepts text input passed to it from other apps. I use [Bear](https://bear.app) for creating posts, mostly.
--- blog.sh
+++ blog.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
-# This script publishes content to my blog.
+# This script publishes content to a blog.
 
-# load the config
-source config
+# load the config.
+# May be specified as the first argument
+# otherwise assume in CWD
+if [[ -n "$1" ]]; then
+  if [[ ! -e "$1" ]]; then
+    echo "Config file not found. Exiting!"
+    exit 1
+  fi
+  source $1
+else
+  if [[ ! -e "config" ]]; then
+    echo "No config found. Exiting!"
+    exit 1
+  fi
+  source config
+fi
 
-# This script relies on GNU date amd sed.
+# This script relies on GNU date and sed.
 # The BSD variants on OSX are not compatible.
-case $(uname) in
-  'Darwin')
-    shopt -s expand_aliases
-    alias sed=gsed
-    alias date=gdate
-  ;;
-esac
+if [[ $(uname) == 'Darwin' ]]; then
+  shopt -s expand_aliases
+  alias sed=gsed
+  alias date=gdate
+fi
 
 ## These variables will be used throughout the script
 # today's date
@@ -29,235 +41,113 @@ DD=${DATE:8:2}
 # remove colons from TIME
 HHMM=${TIME/:/}
 
-#
-##### FUNCTIONS
-#
-# generate an RSS feed of the last 10 items of the specified content type
-function rss {
-  if [[ -z "$1" ]]; then
-    # no input supplied, so generate the default feed
-    TARGET="${DOCROOT}/index.xml"
-    FEED="${URL}/index.xml"
-    SOURCE="all"
-  else
-    # generate the content-specific feed
-    TARGET="${DOCROOT}/${1}s.xml"
-    FEED="${URL}/${1}s.xml"
-    SOURCE="${1}s"
-  fi
-  echo '<?xml version="1.0"?><rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"><channel>' > $TARGET
-  echo '<title>skippy.net</title><link>https://skippy.net/</link>' >> $TARGET
-  echo "<description>${DESCRIPTION}</description><language>en-us</language>" >> $TARGET
-  echo "<pubDate>$(date -R)</pubDate><generator>skippy</generator>" >> $TARGET
-  echo "<atom:link href=\"${FEED}\" rel=\"self\" type=\"application/rss+xml\" />" >> $TARGET
-  for i in $(tail -n 10 $SOURCE.txt | tac); do
-    FILE="$DOCROOT/$i.md"
-    if [[ ! -e "$FILE" ]]; then continue; fi
-    GUID="${URL}/${i}"
-    DATESTRING=$(sed -n 's/^date: //p' $FILE)
-    ITEMDATE=$(date -R -d "$DATESTRING")
-    TITLE=$(sed -n 's/^title: //p' $FILE)
-    # get the content.  Filter through pandoc to get HTML
-    CONTENT=$(sed '/---/,/---/ d' $FILE | pandoc -f markdown -t html)
-    echo -n '<item>' >> $TARGET
-    if [[ -n "$TITLE" ]]; then
-      echo -n "<title>${TITLE}</title>" >> $TARGET
+# we should have a Markdown file in our SRC directory
+COUNT=0
+for SOURCE in ${SRC}/*.md; do
+  if [[ ! -e "${SOURCE}" ]]; then continue; fi
+  (( COUNT=COUNT+1 ))
+  TITLE=$(sed -n 's/^title: //p' $SOURCE)
+  if [[ -n "${TITLE}" ]]; then
+    # use the title for the permalink
+    PERMALINK=$(echo $TITLE | tr '[:upper:]' '[:lower:]' | tr -d [:punct:] | tr ' ' '-')
+    # does a post exist with this permalink?
+    if [[ -e "${DOCROOT}/${PERMALINK}.md" ]]; then
+      # append an incremental suffic to ensure unique permalinks and file names
+      SUFFIX=1
+      while [ -e "${DOCROOT}/${PERMALINK}-${SUFFIX}.md" ]; do
+        (( SUFFIX=SUFFIX+1 ))
+      done
+      PERMALINK+="-${SUFFIX}"
     fi
-    echo "<link>${GUID}</link><description><![CDATA[${CONTENT}]]></description><pubDate>${ITEMDATE}</pubDate><guid>${GUID}</guid></item>" >> $TARGET
-  done
-  echo '</channel></rss>' >> $TARGET
-}
+  else
+    # use the date for the permalink
+    PERMALINK="${YYYYMM}/${DD}/${HHMM}"
+    # does the YYYY/MM/DD directory exist?
+    if [[ ! -d "${DOCROOT}/${YYYYMM}/${DD}" ]]; then
+      # the target directory doesn't exist, so create it.
+      mkdir -p ${DOCROOT}/${YYYYMM}/${DD}
+    else
+      # the directory already exists, so let's make sure
+      # we don't have a file name conflict.
+      if [[ -e "${DOCROOT}/${PERMALINK}.md" ]]; then
+        # we have a file with this HHMM name. Let's get more granular.
+        SECONDS=$(date +%S)
+        PERMALINK+="${SECONDS}"
+        if [[ -e "${DOCROOT}/${PERMALINK}.md" ]]; then
+          # what to do? This should never happen, but bail out if it does
+          echo "A file exists for this timestamp!"
+          exit 1
+        fi
+        TIME+=":${SECONDS}"
+        HHMM+=$SECONDS
+      fi
+    fi
+  fi
+  # replace the permalink placeholder with the real link
+  # use @ instead of / so sed doesn't puke on directory names
+  sed -i "s@PERMALINK@${PERMALINK}@" $SOURCE
+  mv $SOURCE ${DOCROOT}/${PERMALINK}.md
 
-# generate this month's index of notes
-function monthly_note_index {
-  INDEX="${DOCROOT}/${YYYYMM}/index.md"
-  # get a nice display of this date
-  PUBDATE=$(date -d "${YYYYMM}/01" +"%B %Y")
-  echo -e "---\ndate: ${PUBDATE}\n---# ${PUBDATE}" >> $INDEX
-  for i in $(grep ${YYYYMM} notes.txt | tac); do
-    echo "---" >> $INDEX
-    # don't re-use PERMALINK here, so we don't clobber
-    # that value from global scope
-    LINK="${URL}/${i}"
-    file="${DOCROOT}/${i}.md"
-    if [[ ! -e "$file" ]]; then continue; fi
-    PUBDATE=$(sed -n 's/^date: //p' ${file})
-    echo "[&para;](${LINK} '${PUBDATE}') " >> $INDEX
-    sed -e '/---/,/---/d' -e 's/#/\&num;/g' $file >> $INDEX
-  done;
-}
+  # insert this item to the list of all content
+  # use @ as delimiter so that sed doesn't process slashes in permalink
+  sed -i "1s@^@$PERMALINK\n@" $DOCROOT/permalinks.txt
+
+  # update the /archive listing, too
+  # if we have a custom archive function, invoke that
+  if [[ -n "$ARCHIVE_FUNC" ]]; then
+    # pass permalink, date and title to the function
+    $ARCHIVE_FUNC $PERMALINK "$TITLE"
+  else 
+    sed -i "1s@^@* ${PERMALINK}\n@" $DOCROOT/archive.md
+  fi
+done
+
+# generate an RSS feed of the last 10 items
+TARGET="${DOCROOT}/index.xml"
+FEED="${URL}/index.xml"
+echo '<?xml version="1.0"?><rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"><channel>' > $TARGET
+echo "<title>skippy.net</title><link>${URL}</link>" >> $TARGET
+echo "<description>${DESCRIPTION}</description><language>en-us</language>" >> $TARGET
+echo "<pubDate>$(date -R)</pubDate><generator>skippy</generator>" >> $TARGET
+echo "<atom:link href=\"${FEED}\" rel=\"self\" type=\"application/rss+xml\" />" >> $TARGET
+for i in $(head -n ${RSSCOUNT} ${DOCROOT}/permalinks.txt); do
+  FILE="$DOCROOT/$i.md"
+  if [[ ! -e "$FILE" ]]; then continue; fi
+  GUID="${URL}/${i}"
+  DATESTRING=$(sed -n 's/^date: //p' $FILE)
+  ITEMDATE=$(date -R -d "$DATESTRING")
+  TITLE=$(sed -n 's/^title: //p' $FILE)
+  # get the content.  Filter through pandoc to get HTML
+  CONTENT=$(sed '/---/,/---/ d' $FILE | pandoc -f markdown -t html)
+  echo -n '<item>' >> $TARGET
+  if [[ -n "$TITLE" ]]; then
+    echo -n "<title>${TITLE}</title>" >> $TARGET
+  fi
+  echo "<link>${GUID}</link><description><![CDATA[${CONTENT}]]></description><pubDate>${ITEMDATE}</pubDate><guid>${GUID}</guid></item>" >> $TARGET
+done
+echo '</channel></rss>' >> $TARGET
 
 # update the home page
-function update_home {
+if [[ -n "$HOME_FUNC" ]]; then
+  # run the custom home page function
+  $HOME_FUNC
+else
   INDEX="${DOCROOT}/index.md"
   echo -e "---\ntemplate: home\n---" > $INDEX
-  echo "## Recent posts" >> $INDEX
-  for i in $(tail -n 10 posts.txt | tac ); do
-    file="${DOCROOT}/${i}.md"
-    PUBDATE=$(sed -n 's/^date: //p' $file)
-    TITLE=$(sed -n 's/^title: //p' $file | tr -d \"\')
-    echo "* ${PUBDATE} - [${TITLE}](${URL}/${i})" >> $INDEX
-  done
-  echo -e "\n\n---\n## Recent notes" >> $INDEX
-  COUNT=1
-  for i in $(tail -n 5 notes.txt | tac ); do
+  for i in $(head -n ${HOMECOUNT} ${DOCROOT}/permalinks.txt); do
     file="${DOCROOT}/${i}.md"
     if [[ ! -e "$file" ]]; then continue; fi
     LINK="${URL}/${i}"
     PUBDATE=$(sed -n 's/^date: //p' ${file})
-    echo "[&para;](${LINK} '${PUBDATE}') " >> $INDEX
-    # be sure to html-encode hashtags so that Markdown doesn't
-    # try to make them headers!
-    sed -e '/---/,/---/d' -e 's/#/\&num;/g' $file >> $INDEX
-    if [[ $COUNT -lt 5 ]]; then
-      echo -e "\n--\n" >> $INDEX
-      (( COUNT=COUNT+1 ))
-    fi
+    sed -e '/---/,/---/d' $file >> $INDEX
+    echo "[&num;](${LINK} '${PUBDATE}') " >> $INDEX
   done
-}
-
-# copy any uploaded images to the /images/YYYY/MM directory
-# this creates an $IMAGES array with URLs of the images found
-# iterate over images with ${IMAGES[@]} if needed
-function copy_images {
-  IMAGES=() # initialize empty array
-  for i in ${SRC}/*.jpg ${SRC}/*.jpeg; do
-    if [[ -e "$i" ]]; then
-      INAME=$(basename $i)
-      TARGET="${DOCROOT}/images/${YYYYMM}/${INAME}"
-      IMAGES+=("${URL}/images/${YYYYMM}/${INAME}") # add this image to the array
-      if [[ ! -d "${DOCROOT}/images/${YYYYMM}" ]]; then
-        mkdir -p ${DOCROOT}/images/${YYYYMM}
-      fi
-      mv ${i} ${TARGET}
-    fi
-  done
-}
-
-#
-##### Main script
-#
-# we need to be told what to do.
-if [[ -z "$1" ]]; then
-  echo "Need input!"
-  echo "Valid options: post, note, rss"
-  exit 1
 fi
 
-case $1 in
-  'rss')
-    # just regenerate the index.xml feed
-    rss
-    exit 0
-  ;;
-  'home')
-    # regenerate home page
-    update_home
-    exit 0
-  ;;
-  'post'|'note')
-    INPUT=$1
-  ;;
-  *)
-    echo "Unrecognized input"
-    exit 1
-  ;;
-esac
-
-# find and copy any uploaded images
-copy_images
-
-if [[ $INPUT == 'note' ]]; then
-  # this is a note, to be saved at YYYY/MM/DD/HHMM.md
-  ORIGINAL="${SRC}/note.md"
-
-  PERMALINK="${YYYYMM}/${DD}/${HHMM}"
-
-  if [[ ! -d "${DOCROOT}/${YYYYMM}/${DD}" ]]; then
-    # the target directory doesn't exist, so create it.
-    mkdir -p ${DOCROOT}/${YYYYMM}/${DD}
-  else
-    # the directory already exists, so let's make sure
-    # we don't have a file name conflict.
-    if [[ -e "${DOCROOT}/${PERMALINK}.md" ]]; then
-      # we have a file with this HHMM name. Let's get more granular.
-      SECONDS=$(date +%S)
-      PERMALINK+="${SECONDS}"
-      if [[ -e "${DOCROOT}/${PERMALINK}.md" ]]; then
-        # what to do? This should never happen, but bail out if it does
-        echo "A file exists for this timestamp!"
-        exit 1
-      fi
-      TIME+=":${SECONDS}"
-      HHMM+=$SECONDS
-    fi
-  fi
-elif [[ $INPUT == 'post' ]]; then
-  # this is a long-form post.
-  ORIGINAL="${SRC}/post.md"
-  TITLE=$(grep -m 1 '^# ' $ORIGINAL | cut -c 3-)
-  # make a permalink from the title
-  PERMALINK=$(echo $TITLE | tr '[:upper:]' '[:lower:]' | tr -d [:punct:] | tr ' ' '-')
-  # does a post exist with this permalink?
-  if [[ -e "${DOCROOT}/${PERMALINK}.md" ]]; then
-    # append an incremental suffic to ensure unique permalinks and file names
-    SUFFIX=1
-    while [ -e "${DOCROOT}/${PERMALINK}-${SUFFIX}.md" ]; do
-      (( SUFFIX=SUFFIX+1 ))
-    done
-    PERMALINK+="-${SUFFIX}"
-  fi
-  # remove any cruft from Bear
-  sed -i 's/^#blog.*//' $ORIGINAL
+if [[ $COUNT -eq 1 ]]; then
+  # if we had only one file, output a link to that
+  echo $URL/$PERMALINK
 else
-  echo "I have no idea what to do with this!"
-  exit 1
+  # otherwise, just send to the home page
+  echo $URL
 fi
-
-FILE="${DOCROOT}/${PERMALINK}.md"
-echo '---' > $FILE
-if [[ -n "$TITLE" ]]; then
-  echo "title: ${TITLE}" >> $FILE
-fi
-echo "permalink: ${PERMALINK}" >> $FILE
-echo "date: ${DATE} ${TIME}" >> $FILE
-echo '---' >> $FILE
-cat $ORIGINAL >> $FILE
-rm -f $ORIGINAL
-
-# notes get images appended to the end of them
-# posts assume a human being has added the necessary
-# Markdown in the body of the post somewhere.
-if [[ "$INPUT" == 'note' ]] && [ ${#IMAGES[@]} -gt 0 ]; then
-  # we have one or more images. Add Markdown for them in the note.
-  for i in ${IMAGES[@]}; do
-    echo -e "\n\n![](${i})" >> $FILE
-  done
-fi
-
-# add this item to the list of all content
-echo $PERMALINK >> all.txt
-
-# add this item to the content-specific archive
-if [[ $INPUT == 'note' ]]; then
-  echo $PERMALINK >> notes.txt
-  monthly_note_index
-else
-  echo $PERMALINK >> posts.txt
-  # and update the /archive listing, too
-  if [[ $(grep -c "^## ${YYYYMM:0:4}") -eq 0 ]]; then
-    # first entry for this year.
-    echo -e "\n## ${DATE:0:4}" >> $DOCROOT/archive.md
-  fi
-  echo "* ${DATE} - [${TITLE}](/${PERMALINK})" >> $DOCROOT/archive.md
-fi
-
-update_home
-
-# generate RSS feed for the specific content
-rss $INPUT
-# and generate the unified RSS feed
-rss
-
-echo $URL/$PERMALINK
-
